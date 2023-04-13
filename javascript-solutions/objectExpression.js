973dee1c7b37a2a7c3b0aa94ff768da2c301cf44
Kostya Bats <kbats@bk.ru>
2023-04-13 17:11:22 +0300

    Checked by an instructor

--- javascript-solutions/objectExpression.js
+++ javascript-solutions/objectExpression.js
@@ -1,29 +1,26 @@
 //////////////////////////////////////////////////////////// EX 7
 let OPS = {}
 let VAR = {}
-function AbstractExpression() {
-    const abstract = {
-    	toString: function() {
-            return this.value.toString();
-        },
-        prefix: function() {
-            return this.value.toString();
-        }
-    }  
-    return abstract
-} 
+const AbstractExpression = {
+    toString: function () {
+        return this.value.toString();
+    },
+    prefix: function () {
+        return this.value.toString();
+    }
+}
 
-function AbstractOperation (operation, operator, length){
-    const op = function(...args) {
-        this.evaluate = function(x, y, z){
+function AbstractOperation(operation, operator, length) {
+    const op = function (...args) {
+        this.evaluate = function (x, y, z) {
             return operation(...args.map(fn => fn.evaluate(x, y, z)));
         }
-        this.toString = function(){
+        this.toString = function () {
             return args.map(fn => fn.toString()).join(" ") + " " + operator
         }
-        this.prefix = function(){
-            return '(' + operator + " " +args.map(fn => fn.prefix()).join(" ") + ")"
-        }    
+        this.prefix = function () {
+            return '(' + operator + " " + args.map(fn => fn.prefix()).join(" ") + ")"
+        }
     };
 
     OPS[operator] = {func: op, length};
@@ -31,31 +28,28 @@ function AbstractOperation (operation, operator, length){
 }
 
 
-function AbstractVariable()  {
-    const Exp = function (value) {
-    	this.value = value
-        this.evaluate = function(...xyz){
-            return xyz[value.charCodeAt(0) - 'x'.charCodeAt(0)]
-        }
-        
-    }
-    Exp.prototype = Object.create(AbstractExpression());
-    return Exp
-}
 function AbstractConst() {
     const Exp = function (value) {
-    	this.value = value
-        this.evaluate = function(...xyz){
+        this.value = value
+        this.evaluate = function (...xyz) {
             return value
         }
-        
+
     }
-    Exp.prototype = Object.create(AbstractExpression());
+    Exp.prototype = Object.create(AbstractExpression);
     return Exp
 }
-const Const  = AbstractConst()
 
-const Variable =  AbstractVariable()
+const Const = AbstractConst()
+
+function Variable(value) {
+    this.value = value
+    this.evaluate = function (...xyz) { // :NOTE: in prototype
+        return xyz[value.charCodeAt(0) - 'x'.charCodeAt(0)]
+    }
+}
+
+Variable.prototype = Object.create(AbstractExpression);
 VAR.x = 0
 VAR.y = 1
 VAR.z = 2
@@ -82,26 +76,25 @@ const ArcTan2 = AbstractOperation(
     Math.atan2, 'atan2', 2
 )
 const Sum = AbstractOperation(
-    (...args) => args.reduce((total, current) => total + current, 0) , 'sum', 'n'
+    (...args) => args.reduce((total, current) => total + current, 0), 'sum', 'n'
 )
 const Avg = AbstractOperation(
     (...args) => args.reduce((total, current) => total + current, 0) / args.length, 'avg', 'n'
-
 )
-    
+
 
 const parse = expression => {
-  const stack = [];
-  expression.trim().split(/\s+/).forEach(token => {
-    if (token in OPS) {
-      stack.push(new OPS[token].func(...stack.splice(-OPS[token].length)));
-    } else if (token in VAR) {
-      stack.push(new Variable(token));
-    } else {
-      stack.push(new Const(parseFloat(token)));
-    }
-  });
-  return stack[0];
+    const stack = [];
+    expression.trim().split(/\s+/).forEach(token => {
+        if (token in OPS) {
+            stack.push(new OPS[token].func(...stack.splice(-OPS[token].length)));
+        } else if (token in VAR) {
+            stack.push(new Variable(token));
+        } else {
+            stack.push(new Const(parseFloat(token)));
+        }
+    });
+    return stack[0];
 };
 
 
@@ -110,39 +103,41 @@ const parse = expression => {
 const isNumeric = n => !isNaN(n);
 
 const parsePrefix = expression => {
-    if (expression.length === 0){
-	throw new Error("Empty input")
+    if (expression.length === 0) {
+        throw new Error("Empty input")
     }
     const tokens = expression.replace(/(\()|(\))/g, ' $& ').trim().split(/\s+/)
     return parseExpression(tokens)
-    
+
 }
+
 function ParseError(message) {
     this.message = message;
-    
+
 }
+
 ParseError.prototype = new Error();
 
-function Source (expression) {
+function Source(expression) {
     const tokens = expression;
     let pos = -1;
-    
+
     this.getNextToken = () => {
         pos++
         return tokens[pos]
-        
+
     }
     this.getCurrentToken = () => {
         return tokens[pos];
     }
     this.getPosition = () => {
-    	return pos
+        return pos
     }
     this.testNextToken = () => {
-        return tokens[pos+1];
+        return tokens[pos + 1];
     }
     this.eof = () => {
-        return pos === tokens.length-1;
+        return pos === tokens.length - 1;
     }
 }
 
@@ -154,9 +149,10 @@ function parseExpression(expression) {
         throw new ParseError("Expected eof on position on position" + source.getPosition())
     }
     return ans;
-    function parseInsideExpression () {        
-        const token = source.getNextToken();       
-        if (token === "(") {           
+
+    function parseInsideExpression() {
+        const token = source.getNextToken();
+        if (token === "(") {
             let operation = parseOperation();
             if (source.getNextToken() !== ")") {
                 throw new ParseError("Expected closed brackets on position" + source.getPosition());
@@ -169,14 +165,15 @@ function parseExpression(expression) {
         }
         throw new ParseError("Not correcr token or not expected EOF on position" + source.getPosition());
     }
+
     function parseOperation() {
         const operator = parseOperator();
-        const args = parseArgs();   
-        
+        const args = parseArgs();
+
         if (args.length !== operator.length && operator.length !== 'n') {
             throw new ParseError("Not correct number of args for operation")
         }
-        
+
         return new operator.func(...args);
     }
 
